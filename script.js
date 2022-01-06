c00f1a4ffa6274d67ad34d4deed63ef1787c6806
chloeebubedike <chloeebubedike@yahoo.com>
2022-01-06 10:06:31 +0000

    JS test

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 // Get input value after user clicks on submit button
 
-document.getElementById("submit").addEventListender("click",)
+document.getElementById("submit").addEventListener("click", function addToList(){
+    document.getElementById("addList").innerHTML = "Hello World";
+
+}
+);
+
+const body = document.body
+body.append("Hello World")
